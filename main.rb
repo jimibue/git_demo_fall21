90efe87747f6ff2c6f28528d9711cc3c6dd0d647
James Yeates <jimibue@gmail.com>
2021-08-20 11:14:54 -0600

    comments

--- main.rb
+++ main.rb
@@ -20,3 +20,16 @@ end
 def times(num1, num2)
   num1 * num2
 end
+
+# if there is not a repo
+# git init once to initialze the repo (rails we this for us)
+
+# once you start a project it good habit to do you init commit
+# git add .
+# git commit -m'init commit'
+
+# start working on features
+# pick a chuck of work to do, get it working
+# git add .  or (files you want to commit)
+# git commit -m 'describe the work you did'
+# repeat this
