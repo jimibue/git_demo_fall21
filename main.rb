4ff731bdbfd00e11e7f6c3b9b8345d2a45396862
James Yeates <jimibue@gmail.com>
2021-08-20 10:55:26 -0600

    added comments

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
 puts "hello"
+
+# add: stages changes gets them to be commited
+# git add . to stage all changes
+# get add filename to stage indivual changes
+
+# git commit -m 'describe what you did since last commit'
+
+## create a function that adds two numbers
